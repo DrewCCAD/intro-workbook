93474f5cb7d7790486655748fd59289ed52b3a0f
DrewCCAD <drewccad@gmail.com>
2016-09-24 15:01:53 -0500

    lesson 16 in-class
    new script

--- 16LessonSixteen/in-class-project/js/script.js
+++ 16LessonSixteen/in-class-project/js/script.js
@@ -1,12 +1,48 @@
-var divs = $('div[id^="content-"]').hide(),
+var divs = $('.rotating-item').addClass('animated').hide(),
     i = 0;
 
-(function cycle() {
+function cycle() {
 
-    divs.eq(i).fadeIn(400)
-              .delay(2000)
-              .fadeOut(400, cycle);
+    var $div = divs.eq(i).show().removeClass('slideOutLeft')
+              .addClass('slideInRight');
+
+    setTimeout(function() {
+      if (int) {
+        $div.removeClass('slideInRight')
+        .addClass('slideOutLeft');
+      }
+    }, 3000)
+
 
     i = ++i % divs.length;
+}
+var int;
+function startSlideShow() {
+  int = setInterval(cycle, 4000);
+}
+startSlideShow();
+// divs.click(function( ){
+//   if (int) {
+//     clearInterval(int);
+//     int = 0;
+//   } else {
+//     startSlideShow();
+//   }
+// });
 
-})();
+// var divs = $('div[id^="content-"]'),
+//     i = 0;
+//
+// var interval = setInterval(function(){
+//       divs.removeClass('active').removeClass('in');
+//       var $div = divs.eq(i).hide().addClass('in');
+//       $div.show();
+//       requestAnimationFrame(function() {
+//         setTimeout(function( ){
+//           requestAnimationFrame(function() {
+//             $div.addClass('active');
+//           });
+//           i = ++i % divs.length;
+//         }, 100);
+//       });
+//     }, 2000);
